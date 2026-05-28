86b26ec9d390fc7b75e1e136d6ae5495583688f2
deepak <coaderboss@gmail.com>
2026-05-28 14:59:58 +0530

    version 3.001

--- sw.js
+++ sw.js
@@ -1,15 +1,40 @@
+const CACHE_NAME = 'examitop-store-v2';
+
 self.addEventListener('install', (e) => {
+    self.skipWaiting(); // Naya SW aate hi purane ko hata dega
     e.waitUntil(
-        caches.open('examitop-store').then((cache) => cache.addAll([
+        caches.open(CACHE_NAME).then((cache) => cache.addAll([
             '/',
             '/index.html',
-            '/css/exam.css'
+            '/css/exam_v2.css' // Nayi CSS file ka naam
         ]))
     );
 });
 
+self.addEventListener('activate', (e) => {
+    // Purane kachre (caches) ko delete karne ka logic
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
+    );
+});
+
 self.addEventListener('fetch', (e) => {
+    // Smart Fetch: Pehle internet se nayi file laane ki koshish karega
     e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
+        fetch(e.request)
+            .then((response) => {
+                // Agar internet chal raha hai toh nayi file cache me update kar dega
+                const resClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+                return response;
+            })
+            .catch(() => {
+                // Agar internet band hai toh purani saved file dikhayega
+                return caches.match(e.request);
+            })
     );
 });
