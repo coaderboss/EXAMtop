fb2ab2eef9a3420f0c4a63e25b15c2551f8a92a0
deepak <coaderboss@gmail.com>
2026-09-09 18:32:57 +0530

    deploy9

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   
-  //Firebase admin ko bundle hone se rokne ke liye
-  serverExternalPackages: ['firebase-admin'],
+  // 🔥 THE ULTIMATE FIX: Block firebase-admin AND its inner ESM dependencies
+  serverExternalPackages: ['firebase-admin', 'jose', 'jwks-rsa'],
 };
 
 export default nextConfig;
